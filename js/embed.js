59ec72e24816ca172babc1338d6b91a3a6cc5467
xoxoxo <radoslaw@fateone.com>
2016-01-28 19:11:01 +0100

    fix wrong version in embed

--- js/embed.js
+++ js/embed.js
@@ -1,7 +1,7 @@
 letSlide.embed = function() {
   var embedNode = document.createElement('div');
   embedNode.setAttribute('id', 'letSlide-share');
-  var embedCode = document.createTextNode('<div data-letSlidejs="' + letSlide.getletSlideUrl + '"></div> <script src="//d3gr29hczmiozh.cloudfront.net/0.1.3/letSlide.min.js" async></script>');
+  var embedCode = document.createTextNode('<div data-letSlidejs="' + letSlide.getletSlideUrl + '"></div> <script src="//d3gr29hczmiozh.cloudfront.net/0.1.5/letSlide.min.js" async></script>');
   embedNode.innerHTML = '<div id="letSlide-embed-close">x</div><div id="letSlide-title">Embed<p>Copy and paste the code below into your website</p></div><textarea id="letSlide-code"></textarea>';
   letSlide.letSlideContainer.appendChild(embedNode);
   document.getElementById('letSlide-code').appendChild(embedCode);
