fb4285b0ee2455ba78fc579e31929878d29713f6
xoxoxo <radoslaw@fateone.com>
2016-03-10 18:49:20 +0100

    change preloader icon name

--- build/letSlide.dev.js
+++ build/letSlide.dev.js
@@ -213,12 +213,12 @@ letSlide.checkifready = function(){
 };
 
 var insertSpiner = document.createElement('style');
-insertSpiner.innerHTML = '[data-letSlidejs] {font-family: Helvetica, Arial, sans-serif;position:relative;height:500px}#letSlide-preloader{position:absolute;top:0;bottom:0;left:0;right:0;padding:15% 0 0;background:#fff;z-index:50;color:#000;text-align:center}#letSlide-preloader:after{content:"Loading, please wait...";font-size:12px;font-weight:100;display:block}@keyframes letSlideSpinner{from{transform:rotate(0deg)}to{transform:rotate(360deg)}}@-webkit-keyframes letSlideSpinner{from{-webkit-transform:rotate(0deg)}to{-webkit-transform:rotate(360deg)}}.icon-spinner{-webkit-animation:letSlideSpinner .75s linear infinite;animation:letSlideSpinner 2s linear infinite;font-size:20px;line-height:50px;width:50px;height:50px;cursor:default;text-align:center;color:#000}';
+insertSpiner.innerHTML = '[data-letSlidejs] {font-family: Helvetica, Arial, sans-serif;position:relative;height:500px}#letSlide-preloader{position:absolute;top:0;bottom:0;left:0;right:0;padding:15% 0 0;background:#fff;z-index:50;color:#000;text-align:center}#letSlide-preloader:after{content:"Loading, please wait...";font-size:12px;font-weight:100;display:block}@keyframes letSlideSpinner{from{transform:rotate(0deg)}to{transform:rotate(360deg)}}@-webkit-keyframes letSlideSpinner{from{-webkit-transform:rotate(0deg)}to{-webkit-transform:rotate(360deg)}}.slideme-icon-spinner{-webkit-animation:letSlideSpinner .75s linear infinite;animation:letSlideSpinner 2s linear infinite;font-size:20px;line-height:50px;width:50px;height:50px;cursor:default;text-align:center;color:#000}';
 letSlide.getHead.appendChild(insertSpiner);
 
 letSlide.addPreloader = function () {
 
-  var preloaderDom = '<i class="icon-spinner">.</i>';
+  var preloaderDom = '<i class="slideme-icon-spinner">.</i>';
   letSlide.DOM.preloaderWrapper = document.createElement('div');
   letSlide.DOM.preloaderWrapper.setAttribute('id', 'letSlide-preloader');
   letSlide.DOM.preloaderWrapper.innerHTML = preloaderDom;
